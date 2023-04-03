34032a78b77680429e6ade49ba70cec823375e1c
felipesandoli <cfelipesandoli@gmail.com>
2023-04-03 06:33:27 +0000

    Fix env import and start server

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 import os
 
 if os.path.exists("env.py"):
-    import env.py
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
